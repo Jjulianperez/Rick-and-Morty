488a7d9969ffb7851912095c867b6c768a36c3e7
Julian Perez <131901763+Jjulianperez@users.noreply.github.com>
2025-09-19 14:38:50 -0300

    fix: corregi errores en configuración de la rutas refactor: Acomo de el footer y le agregue sus clases

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,35 +1,40 @@
-import { Outlet, Link } from "@tanstack/react-router";
-
-// export const Route = () => {
-//   return (
-//     <div>
-//       <nav style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
-//         <Link to="/index.tsx">Home</Link>
-//         <Link to="/favoritos">Favoritos</Link>
-//       </nav>
-//       <Outlet /> {
-//         <h1>funcionalidad de las rutas</h1>
-//       }
-//     </div>
-//   );
-// };
-
-import { createRootRoute } from "@tanstack/react-router";
-
+import { createRootRoute, Outlet, Link } from "@tanstack/react-router";
+import ImgTitulo from "../assets/tituloRickAndMorty.png";
+import ImgIcono from "../assets/Icono.png";
+import "../styles/main.scss";
 
 const RootRouteComponent = () => {
-    return (
-    <div>
-      <nav style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
+  return (
+    <>
+    <section className="layout">
+      <header className="header">
+          <div className="portalIcono">
+            <img src={ImgIcono} alt="Rick And Morty Portal" />
+          </div>
+          <div className="tituloPrincipal">
+            <img src={ImgTitulo} alt="Rick and Morty" />
+          </div>
+      </header>
+
+      <nav className="navbar">
         <Link to="/">Home</Link>
         <Link to="/favoritos">Favoritos</Link>
       </nav>
-      <Outlet /> {
-        <h1>funcionalidad de las rutas</h1>
-      }
-    </div>
-  )
+
+      <main>
+        <Outlet />
+      </main>
+      <footer className="footer"> 
+      <p>
+        <span className="emoji">💻</span> Desarrollado por <b>JulianDev</b> |{" "} 
+        <span className="emoji">🚀</span> Pasante en Bewise 
+      </p> 
+      </footer>
+    </section>
+      </>
+  );
 };
+
 export const Route = createRootRoute({
-          component: RootRouteComponent,
-        });
+  component: RootRouteComponent,
+});
