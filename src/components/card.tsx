6d27ba31d78dcb5ea0ad4370d6d9b9e35d052fb0
Julian Perez <131901763+Jjulianperez@users.noreply.github.com>
2025-09-19 14:29:58 -0300

    feat: creé el componente Card para mostrar la información de los personajes

---
+++ src/components/card.tsx
@@ -0,0 +1,20 @@
+import ImgCharacter from '../assets/cargando.png'
+
+export const Card = ({isFav}) =>{
+    return(
+        <article className={isFav}>
+            <header className="imagen">
+                <img src={ImgCharacter} alt="" />
+            </header>
+            <div className="infoPersonaje">
+                <strong><p>Nombre</p></strong>
+                <strong><p>Estado</p></strong>
+                <p>Visto por Primera vez en la tierra</p>
+                <p>Visto por ultima vez en el lago</p>
+            </div>
+            <footer>
+                <button>Ver Personaje</button>
+            </footer>
+        </article>
+    )
+}
