c4a1d227478ecc014dac52e5eb2d358b6d83658a
Julian Perez <131901763+Jjulianperez@users.noreply.github.com>
2025-09-19 14:43:29 -0300

    feat: agregue importaciónes de los componentes Filtro y Card style: importe y aplique estilos desde main.scss

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Card } from '../components/card'
+import { Filtro } from '../components/filtro'
+import "../styles/main.scss"
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  return (
+    <main>
+      <Filtro/>
+        <div className='cards-grid'>
+          <Card isFav={"character-card"} />
+          <Card isFav={"character-card-fav"} />
+          <Card isFav={"character-card"} />
+          <Card isFav={"character-card-create"} />
+          <Card isFav={"character-card-fav"} />
+          <Card isFav={"character-card"} />
+          <Card isFav={"character-card"} />
+          <Card isFav={"character-card"} />
+        </div>
+    </main>
+  )
 }
