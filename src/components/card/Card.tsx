d1cebb693f47489f02a73e8d1214437a612b0f91
Julian Perez <131901763+Jjulianperez@users.noreply.github.com>
2025-09-19 17:11:29 -0300

    feat: Agregue los tipos al componente y de paso le puse algunos datos en los parametros

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,18 +1,27 @@
-export const Card = ({className,ImgCharacter}) =>{
-    return(
-        <article className={className}>
-            <header className="imagen">
-                <img src={ImgCharacter} alt="" />
-            </header>
-            <div className="infoPersonaje">
-                <strong><p>Nombre</p></strong>
-                <strong><p>Estado</p></strong>
-                <p>Visto por Primera vez en la tierra</p>
-                <p>Visto por ultima vez en el lago</p>
-            </div>
-            <footer>
-                <button>Ver Personaje</button>
-            </footer>
-        </article>
-    )
-}
+type CardProps = {
+  className?: string;
+  ImgCharacter: string;
+  name: string;
+  status: string;
+  firstSeen: string;
+  lastSeen: string;
+};
+
+export const Card = ({ className = "character-card", ImgCharacter, name, status, firstSeen, lastSeen }: CardProps) => {
+  return (
+    <article className={className}>
+      <img src={ImgCharacter} alt={name} />
+      <div className="infoPersonaje">
+        <div>
+          <p className="name">{name}</p>
+          <span className="status">{status}</span>
+          <p className="text">Visto por primera vez en {firstSeen}</p>
+          <p className="text">Visto por última vez en {lastSeen}</p>
+        </div>
+        <footer>
+          <button>Ver Personaje</button>
+        </footer>
+      </div>
+    </article>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -22,14 +22,12 @@ function RouteComponent() {
     <main>
       <Filtro/>
         <div className='cards-grid'>
-          <Card className={"character-card"} ImgCharacter={ImgCharacter} />
-          <Card className={"character-card-fav"} ImgCharacter={ImgCharacter1} />
-          <Card className={"character-card"} ImgCharacter={ImgCharacter2} />
-          <Card className={"character-card-create"} ImgCharacter={ImgCharacter3} />
-          <Card className={"character-card-fav"} ImgCharacter={ImgCharacter4} />
-          <Card className={"character-card"} ImgCharacter={ImgCharacter5} />
-          <Card className={"character-card"} ImgCharacter={ImgCharacter} />
-          <Card className={"character-card"} ImgCharacter={ImgCharacter} />
+          <Card className={"character-card"} ImgCharacter={ImgCharacter} name='JULIAN PEREZ' status='dead' firstSeen='EL PARAISO' lastSeen='aqui'/>
+          <Card className={"character-card fav"} ImgCharacter={ImgCharacter1} name='juan123' status='live'firstSeen='MI CASA' lastSeen='unknow' />
+          <Card className={"character-card"} ImgCharacter={ImgCharacter2} name='belu' status='live' firstSeen='el primero' lastSeen='en el ultimo' />
+          <Card className={"character-card create"} ImgCharacter={ImgCharacter3} name='carlos' status='unknow' firstSeen='el 15' lastSeen='ultimo episodio' />
+          <Card className={"character-card fav"} ImgCharacter={ImgCharacter4} name='Juli' status='dead' firstSeen='en el segundo' lastSeen='penultimo' />
+          <Card className={"character-card"} ImgCharacter={ImgCharacter5} name='elsebas' status='live' firstSeen='en el primero'lastSeen='en el ultimo' />
         </div>
     </main>
   )
