6b6d0c43ec7bf81b84ef7f8c11ccecf503ccdff8
Julian Perez <131901763+Jjulianperez@users.noreply.github.com>
2025-09-19 16:19:58 -0300

    feat: Cambie el isFav por className y las imagenes de prueba

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,16 @@ import { Card } from '../components/card/Card'
 import { Filtro } from '../components/filtro/Filtro'
 import "../styles/main.scss"
 
+
+//-----------fotos de prueba xd---------------
+import ImgCharacter from '../assets/cargando.png'
+import ImgCharacter1 from '../assets/1.jpeg'
+import ImgCharacter2 from '../assets/2.jpeg'
+import ImgCharacter3 from '../assets/3.jpeg'
+import ImgCharacter4 from '../assets/4.jpeg'
+import ImgCharacter5 from '../assets/5.jpeg'
+// ------------------------------------------
+
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
@@ -12,14 +22,14 @@ function RouteComponent() {
     <main>
       <Filtro/>
         <div className='cards-grid'>
-          <Card isFav={"character-card"} />
-          <Card isFav={"character-card-fav"} />
-          <Card isFav={"character-card"} />
-          <Card isFav={"character-card-create"} />
-          <Card isFav={"character-card-fav"} />
-          <Card isFav={"character-card"} />
-          <Card isFav={"character-card"} />
-          <Card isFav={"character-card"} />
+          <Card className={"character-card"} ImgCharacter={ImgCharacter} />
+          <Card className={"character-card-fav"} ImgCharacter={ImgCharacter1} />
+          <Card className={"character-card"} ImgCharacter={ImgCharacter2} />
+          <Card className={"character-card-create"} ImgCharacter={ImgCharacter3} />
+          <Card className={"character-card-fav"} ImgCharacter={ImgCharacter4} />
+          <Card className={"character-card"} ImgCharacter={ImgCharacter5} />
+          <Card className={"character-card"} ImgCharacter={ImgCharacter} />
+          <Card className={"character-card"} ImgCharacter={ImgCharacter} />
         </div>
     </main>
   )
