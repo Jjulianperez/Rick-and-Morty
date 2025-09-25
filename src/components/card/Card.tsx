a881d8f9b9da05589590453e99d4fd2564852ff3
Julian Perez <131901763+Jjulianperez@users.noreply.github.com>
2025-09-25 15:18:27 -0300

    refactor: actualizar props del componente Card feat: agregar botón de favoritos en el componente Card y importe Hook useState refactor: reorganizar funciones para mejorar legibilidad

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+
 type CardProps = {
-  className?: string;
+  isFavorito: boolean;
+  isCreate: boolean;
   ImgCharacter: string;
   name: string;
   status: string;
@@ -7,19 +11,40 @@ type CardProps = {
   lastSeen: string;
 };
 
-export const Card = ({ className = "character-card", ImgCharacter, name, status, firstSeen, lastSeen }: CardProps) => {
+export const Card = ({ isFavorito, isCreate, ImgCharacter, name, status, firstSeen, lastSeen,}: CardProps) => {
+
+  const [isFav, setIsFav] = useState(isFavorito);
+
+
+let className = "character-card";
+  
+if (isFav && isCreate) className += " createAndFav";
+
+else if (isFav) className += " fav";
+
+else if (isCreate) className += " create";
+
+  const handleFav = () => {
+    console.log(`Este es el valor antes del click ${isFav}`)
+    
+    setIsFav(!isFav)
+
+    console.log(`Este es el valor despues del click ${isFav}`)
+  };
+
   return (
     <article className={className}>
       <img src={ImgCharacter} alt={name} />
       <div className="infoPersonaje">
         <div>
           <p className="name">{name}</p>
-          <span className="status">{status}</span>
+          <span className={`status ${status}`}>{status}</span>
           <p className="text">Visto por primera vez en {firstSeen}</p>
           <p className="text">Visto por última vez en {lastSeen}</p>
         </div>
         <footer>
           <button>Ver Personaje</button>
+          <AiOutlineHeart onClick={handleFav}>Agregar favoritos</AiOutlineHeart>
         </footer>
       </div>
     </article>
