e2feb818bba67a6e0339376edce183c63390ebd9
Julian Perez <131901763+Jjulianperez@users.noreply.github.com>
2025-09-26 16:35:10 -0300

    Refactor: Se agrego todos los selects dentro de un div para poder acomodarlos con los estilos

--- src/components/filtro/Filtro.tsx
+++ src/components/filtro/Filtro.tsx
@@ -1,30 +1,31 @@
 import { Select } from '@headlessui/react'
 
 export const Filtro = () =>{
-    return(
-        
-        <article className='filters'>
+  return(
+    <article className='filters'>
+      <div className='search'>
+        <input type="text" placeholder='Buscar Personaje' />
+      </div>
 
-            <div className='search'>
-                <input type="text" placeholder='Buscar Personaje' />
-            </div>
-            
-            <Select name="status" id="status">
-                <option value="live">Vivo</option>
-                <option value="Human-with-antennae">Human with antennae</option>
-            </Select>
+      <div className="filters-selects">
+        <Select name="status" id="status">
+          <option value="live">Vivo</option>
+          <option value="dead">Muerto</option>
+          <option value="unknown">Desconocido</option>
+        </Select>
 
-            <Select name="species" id="species">
-                <option value="human">Humano</option>
-                <option value="alien">Alien</option>
-                <option value="unknown">Desconocido</option>
-            </Select>
+        <Select name="species" id="species">
+          <option value="human">Humano</option>
+          <option value="alien">Alien</option>
+          <option value="unknown">Desconocido</option>
+        </Select>
 
-            <Select name="status" id="status">
-                <option value="live">Vivo</option>
-                <option value="dead">Muerto</option>
-                <option value="unknown">Desconocido</option>
-            </Select>
-        </article>
-    )
+        <Select name="type" id="type">
+          <option value="default">Tipo</option>
+          <option value="robot">Robot</option>
+          <option value="parasite">Parásito</option>
+        </Select>
+      </div>
+    </article>
+  )
 }
