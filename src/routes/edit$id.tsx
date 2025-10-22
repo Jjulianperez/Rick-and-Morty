1cb02e2a5a8402633371fc516c768f28863a6016
Julian Perez <131901763+Jjulianperez@users.noreply.github.com>
2025-10-22 15:09:04 -0300

    feat: Se creo la direccionde el Edit character

---
+++ src/routes/edit$id.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { EditForm } from '../components/editForm/EditForm'
+
+export const Route = createFileRoute('/edit$id')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <EditForm/>
+}
