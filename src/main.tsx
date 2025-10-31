d92f23b10aa2e67d3acf5e3abce861f9adde4b8c
Julian Perez <131901763+Jjulianperez@users.noreply.github.com>
2025-10-31 14:33:00 -0300

    Feat: se agrego el QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import {
   RouterProvider,
   createRouter,
@@ -8,6 +10,14 @@ import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      gcTime: 1000 * 60 * 10, // 10 minutes
+    },
+  },
+})
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
@@ -15,6 +25,8 @@ declare module "@tanstack/react-router" {
 }
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
